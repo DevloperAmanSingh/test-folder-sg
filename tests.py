b9f68643f401a47ff7bec69ec042fe7538555536
Aman Singh <80804989+DevloperAmanSingh@users.noreply.github.com>
2025-12-24 01:26:42 +0530

    StackGuard Auto-Remediation: Secure secrets in tests.py

--- tests.py
+++ tests.py
@@ -1,1 +1,7 @@
-GITHUB_TOKEN = "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"
+import hvac
+
+client = hvac.Client()
+vault_path = 'secret/data/DevloperAmanSingh/test-folder-sg/tests.py'
+secret_key = 'secret'
+read_response = client.secrets.kv.v2.read_secret_version(path=vault_path)
+GITHUB_TOKEN = read_response['data']['data'][secret_key]
